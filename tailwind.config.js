68a60e5759e6f96e5a6d44b0ae08758f7cb31f98
Yeom-Seung-Ah <ysa2780@naver.com>
2025-09-15 19:35:31 +0900

    build: 폰트 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ['var(--font-heading)', 'system-ui', 'sans-serif'],
+        body: ['var(--font-body)', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
